4a9f509cb09d4b74bb236eaab10ea89566f10aa2
KiJungHyun <90playki@gmail.com>
2019-04-06 16:05:19 +0900

    190406_MS_02

--- PuzzlePlatform/Source/PuzzlePlatform/PuzzlePlatformGameInstance.cpp
+++ PuzzlePlatform/Source/PuzzlePlatform/PuzzlePlatformGameInstance.cpp
@@ -4,16 +4,44 @@
 #include "PuzzlePlatformGameInstance.h"
 #include "Runtime/Engine/Classes/Engine/Engine.h"
 #include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+#include "UObject/ConstructorHelpers.h"
+#include "PlatformTrigger.h"
+#include "RunTime/UMG/Public/Blueprint/UserWidget.h"
 
 
 UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer & FObjectInitializer)
 {
-	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
+	ConstructorHelpers::FClassFinder<UUserWidget> MenuBPClass(TEXT("/Game/MenuSystem/WBP_MainMenu"));
+
+	if (!ensure(MenuBPClass.Class != nullptr)) { return; }
+
+	MenuClass = MenuBPClass.Class;
 }
 
 void UPuzzlePlatformGameInstance::Init()
 {
-	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+	UE_LOG(LogTemp, Warning, TEXT("Found class %s"), *MenuClass->GetName());
+}
+
+void UPuzzlePlatformGameInstance::LoadMenu()
+{
+	if (!ensure(MenuClass != nullptr)) { return; }
+
+	UUserWidget* Menu = CreateWidget<UUserWidget>(this, MenuClass);
+	
+	if (!ensure(Menu != nullptr)) { return; }
+
+	Menu->AddToViewport();
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+
+	if (!ensure(PlayerController != nullptr)) { return; }
+
+	FInputModeUIOnly InputModeData;
+	InputModeData.SetWidgetToFocus(Menu->TakeWidget());
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	PlayerController->SetInputMode(InputModeData);
+	PlayerController->bShowMouseCursor = true;
 }
 
 void UPuzzlePlatformGameInstance::Host()
